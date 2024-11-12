386558f0add5370be21de73d36b4cc9d29266a0e
KamTom <kamiltomala04@gmail.com>
2024-11-12 22:34:02 +0100

    Final commit... added gitignore and commiting final solution

---
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPattern.Factory
+{
+    public static class DatabaseFactory
+    {
+        //public static IDatabase CreateDatabase(...)
+        //{
+        //  ...
+        //}
+
+        public static IDatabase CreateDatabase(DatabaseType type)
+        {
+            if (type == DatabaseType.Oracle) return new OracleDatabase();
+            return new SqlServerDatabase();
+            
+        }
+    }
+}
